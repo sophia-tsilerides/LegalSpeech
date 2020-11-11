3c6a7550ae7509576c5c22861ff800b7d1035197
Amanda Kuznecov <54920249+akuz91@users.noreply.github.com>
2020-11-11 12:20:14 -0500

    updated script description

--- icsi_corpus/del_small_files.py
+++ icsi_corpus/del_small_files.py
@@ -1,5 +1,8 @@
 import os
 
+"""This script points to the location of all speaker files and checks the size of each .wav file. Any audio files <40KB and their corresponding .txt file 
+are removed from the directory because either one or less words are spoken in the segment, or the speech is undetectable and will not be picked up by VAD."""
+
 path = "./icsi_files/"
 folders = os.listdir(path)
 
