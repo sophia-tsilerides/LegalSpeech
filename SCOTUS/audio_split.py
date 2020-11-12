98f74d0ae9ccfd56912763e8357a30565db490f4
Sophia T <smt570@nyu.edu>
2020-11-12 13:50:55 -0500

    refactored code for oyez mp3s, conversion, and splitting

---
+++ SCOTUS/audio_split.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import re
+import os
+
+def getMeta(docket, data):
+    
+    #get meta data as well as rearrange to desirable formal
+    transcript, speakers, speaker_roles, times = data[docket]
+    
+    # Flatten times list
+    times_new = []
+    for t in times:
+        flatten = [item for sublist in t for item in sublist]
+        times_new.append(flatten)  
+    # Last element of list is a 0 - cleanup    
+    del times_new[-1][-1]
+    
+    # Flatten speaker_roles list and replace nulls with "Other"
+    speaker_roles_clean = []
+    for i in speaker_roles:
+        if not i:
+            speaker_roles_clean.append('Other')
+        else:
+            speaker_roles_clean.append(i[0])
+            
+    # Remove all non-word characters in speakers' names
+    speakers =[re.sub(r"[^\w\s]", '', s) for s in speakers]
+    # Replace all runs of whitespace with underscorei in speakers' names
+    speakers =[re.sub(r"\s+", '_', s) for s in speakers]
+    
+    return transcript, speakers, speaker_roles_clean, times_new
+
+def getSpeakerDict(transcript, speakers, speaker_roles, times_new):
+    speaker_dict = {}
+    
+    # Create a dictionary with all the parsed data from getMeta()
+    for i in range(len(speakers)):
+        speaker_dict[speakers[i] + '_' + speaker_roles[i] + '_' + str(i)] = times_new[i][0], times_new[i][-1], transcript[i]
+        
+    return speaker_dict
+
+def createFolders(docket, speakers, speaker_roles, times, data):
+    os.mkdir(os.getcwd() + '/SCOTUS/' + str(docket) + '_SCOTUS')
+    
+    folders = []
+    for i in range(len(speakers)):
+        folders.append(speakers[i] + '_' + speaker_roles[i])
+    folders = list(dict.fromkeys(folders))
+    
+    for f in folders:
+        os.mkdir(os.getcwd() + '/SCOTUS/' + str(docket) + '_SCOTUS/'+f)
+        
+def getSplittingAndWriteCommands(docket, speaker_dict):
+    for k,v in speaker_dict.items():
+
+        # Establish folder
+        folder = '_'.join(k.split('_')[0:-1])
+        file_path_out = os.getcwd() + '/SCOTUS/'+docket+'_SCOTUS/'+folder+'/'+k+'.wav'
+
+        # Split wav and put in the right folder
+        command = 'ffmpeg -ss '+ str(v[0])+' -t '+ str(str(v[1] - v[0]))+' -i '+ docket+'.wav ' + file_path_out
+        os.system(command)
+        
+        # Create .txt 
+        text = v
+        start = str(v[0])
+        stop = str(v[1])
+        # Turn the list of speaker's speech into a string and put it back in the tuple
+        speaker_text = v[2]
+        speaker_text = ' '.join(speaker_text)
+
+        # Write .txt
+        with open(os.getcwd() + '/SCOTUS/'+docket+'_SCOTUS/'+folder+'/'+k+'.txt', "w") as outfile:
+            outfile.write(start + ' ' + stop + ' ' + speaker_text)
+        
+def main_script(file_path = '/oyez_metadata.json'):
+    
+    with open(os.getcwd() + file_path) as f:
+        data = json.load(f)
+
+    for docket in data:
+        #get meta data
+        transcript, speakers, speaker_roles, times_new = getMeta(docket,data)
+
+        #create speaker dict
+        speaker_dict = getSpeakerDict(transcript, speakers, speaker_roles, times_new)
+
+        #create folder for docket and then sub folders (speaker + speaker_role) for each speaker in docket 
+        createFolders(docket, speakers, speaker_roles, times_new, data)
+
+        #split and move to correct folder 
+        getSplittingAndWriteCommands(docket, speaker_dict)
+
+main_script()
