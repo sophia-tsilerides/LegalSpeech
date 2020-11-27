0501ff10e001f3656df7d6e420e7c552341a9b11
JeffT13 <jefftumminia@gmail.com>
2020-11-26 19:08:54 -0500

    UISRNN folder

---
+++ UISRNN/prince_trainuisrnn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8GB
+#SBATCH --job-name=uis_trn
+#SBATCH --output=uisrnn.out
+
+python LegalUISRNN/SCOTUS/trainmod_SCOTUS.py
