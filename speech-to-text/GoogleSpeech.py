c027b08be0dc59057fa29497b78b9795520981d2
Sophia T <smt570@nyu.edu>
2020-12-29 17:28:48 -0500

    Modified to handle dirization and large files

--- speech-to-text/GoogleSpeech.py
+++ speech-to-text/GoogleSpeech.py
@@ -18,11 +18,10 @@
 batch processing.
 
 How to use:
-    python GoogleSpeech4.py Filename.flac
+    python GoogleSpeech.py Filename.flac num_speakers
+    python GoogleSpeech.py gs://cloud-samples-tests/speech/vr.flac num_speakers
 
-Upload file to https://console.cloud.google.com/storage/browser/gsfiles?project=second-form-228820&folder&organizationId=1062515115803
-
-Base code from by Julian Redlich (please@rankmeamadeus.com)
+Base code from github.com/googleapis
 Adjustments by Sophia 
 """
 
@@ -30,13 +29,16 @@ import argparse
 import io
 import os
 
+# TODO: Enable data logging for price discount; (Data logging is disabled for this project for Google Cloud Speech API) 
+#       Speech Adaptation: https://cloud.google.com/speech-to-text/docs/context-strength, https://cloud.google.com/speech-to-text/docs/speech-adaptation
 
 # [START speech_transcribe_async]
-def transcribe_file(speech_file):
+def transcribe_file(speech_file, num_speakers):
     """Transcribe the given audio file asynchronously."""
 
     # Imports the Google Cloud client library
-    from google.cloud import speech
+    #from google.cloud import speech
+    from google.cloud import speech_v1p1beta1 as speech
 
 
     # Instantiates a client
@@ -44,25 +46,116 @@ def transcribe_file(speech_file):
 
     # The name of the audio file to transcribe
     file_name = os.path.join(os.path.dirname(__file__), "resources", "audio.raw")
-
+    
     # Loads the audio into memory
     with io.open(speech_file, "rb") as audio_file:
         content = audio_file.read()
-        audio = speech.RecognitionAudio(content=content)
+    
+    # Construct a recognition metadata object
+    metadata = speech.RecognitionMetadata()
+    metadata.interaction_type = speech.RecognitionMetadata.InteractionType.DISCUSSION
+    metadata.recording_device_type = (
+        speech.RecognitionMetadata.RecordingDeviceType.OTHER_INDOOR_DEVICE
+    )
+    metadata.audio_topic = "court trial hearing" 
+    metadata.original_mime_type = "audio/mp3"
+
+    audio = speech.RecognitionAudio(content=content)
 
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
         sample_rate_hertz=16000,
         language_code="en-US",
+        enable_automatic_punctuation=True,
+        enable_speaker_diarization=True,
+        diarization_speaker_count=num_speakers,
+        # Enhanced models cost more than standard models. 
+        use_enhanced=True,
+        model="video",
+        enable_word_time_offsets=True,
+
+        )
+
+    # Detects speech in the audio file -- short audio file
+    print("Waiting for operation to complete...")
+    response = client.recognize(config=config, audio=audio)
+    result = response.results[-1]
+
+    words_info = result.alternatives[0].words
+
+    # Printing out the output:
+    for word_info in words_info:
+        print(
+            u"word: '{}', speaker_tag: {}, start_time:{}, end_time:{}".format(word_info.word, word_info.speaker_tag, word_info.start_time.total_seconds(), word_info.end_time.total_seconds())
+        )
+    
+# [END speech_transcribe_async]
+
+
+# [START speech_transcribe_async_gcs]
+def transcribe_gcs(gcs_uri, num_speakers):
+    """Asynchronously transcribes the audio file specified by the gcs_uri."""
+
+    # Imports the Google Cloud client library
+    #from google.cloud import speech
+    from google.cloud import speech_v1p1beta1 as speech
+
+
+    # Instantiates a client
+    client = speech.SpeechClient()
+    
+    # Construct a recognition metadata object
+    metadata = speech.RecognitionMetadata()
+    metadata.interaction_type = speech.RecognitionMetadata.InteractionType.DISCUSSION
+    metadata.recording_device_type = (
+        speech.RecognitionMetadata.RecordingDeviceType.OTHER_INDOOR_DEVICE
+    )
+    metadata.audio_topic = "court trial hearing" 
+    metadata.original_mime_type = "audio/mp3"
+
+    audio = speech.RecognitionAudio(uri=gcs_uri)
+
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=16000,
+        language_code="en-US",
+        enable_automatic_punctuation=True,
+        enable_speaker_diarization=True,
+        diarization_speaker_count=num_speakers,
+        # Enhanced models cost more than standard models. 
+        use_enhanced=True,
+        model="video",
+        enable_word_time_offsets=True,
+
     )
 
-    # Detects speech in the audio file
-    response = client.recognize(config=config, audio=audio)
+    # Detects speech in the audio file -- long audio file
+    operation = client.long_running_recognize(config=config, audio=audio)
 
+    print("Waiting for operation to complete...")
+    response = operation.result(timeout=300)
+
+    # Writing results to txt
+    case_file = open("{}.txt".format(gcs_uri.split('/')[-1][:-5]), "a")
     for result in response.results:
-        print("Transcript: {}".format(result.alternatives[0].transcript))
-        print('Confidence: {}'.format(result.alternatives[0].confidence))
+        alternative = result.alternatives[0]
+        case_file.write("Transcript: {} \n".format(alternative.transcript))
+        case_file.write("Confidence: {} \n".format(alternative.confidence))
 
+        for word_info in alternative.words:
+            word = word_info.word
+            start_time = word_info.start_time
+            end_time = word_info.end_time
+            speaker_tag = word_info.speaker_tag
+
+            case_file.write(
+                f"Word: {word}, start_time: {start_time.total_seconds()}, end_time: {end_time.total_seconds()}, speaker_tag: {speaker_tag} \n"
+            )
+    case_file.close()
+
+    print("Dirized and transcribed {}".format(gcs_uri.split('/')[-1]))
+
+# [END speech_transcribe_async_gcs]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -70,5 +163,11 @@ if __name__ == '__main__':
         formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument(
         'path', help='File or GCS path for audio file to be recognized')
+    parser.add_argument(
+        'num_speakers', help='diarization_speaker_count')
     args = parser.parse_args()
-    transcribe_file(args.path)
+
+    if args.path.startswith("gs://"):
+        transcribe_gcs(args.path, int(args.num_speakers))
+    else:
+        transcribe_file(args.path, int(args.num_speakers))
